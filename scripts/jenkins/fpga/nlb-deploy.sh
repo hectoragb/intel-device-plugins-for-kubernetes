385fbca7c2d7ecce50f7ca5d76a0b80745d0f544
Hector Augusto Garcia Baleon <hector.augusto.garcia.baleon@intel.com>
2019-12-13 14:28:24 -0600

    fpga: Added deployment and bare metal testing for fpga plugin

---
+++ scripts/jenkins/fpga/nlb-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Copyright 2019 Intel Corporation.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Deploys current jenkins-build test image for nlb-demo
+# in the cluster.
+
+set -o pipefail
+set -o xtrace
+set -o errexit
+
+REPO_ROOT=$(realpath $(dirname $0)/../../..)
+kubectl create -f ${REPO_ROOT}/demo/test-fpga-region.yaml
+kubectl wait --for=condition=Completed pod --all --timeout=5m  --namespace kube-system && sleep 60s
